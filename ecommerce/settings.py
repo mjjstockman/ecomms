6a8615a0089a001a917c3ac857df37422198b707
mjjstockman <mjjstockman@gmail.com>
2022-09-01 12:08:25 +0000

    Import env if env.py exists

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 import dj_database_url
 
-
+if os.path.exists("env.py"):
+    import env
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -11,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
--- eny.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = '4)fo8j%jeo5)0_h0h*wc&aj+^sc+v$=el9i3am@%_b_v8odm8y'
